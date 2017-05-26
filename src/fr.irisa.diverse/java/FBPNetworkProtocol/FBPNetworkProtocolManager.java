ebe9819d1a7e41891921c92efd069beaca2e90a8
AntoineCheron <cheron.antoine@gmail.com>
2017-05-26 17:56:46 +0200

    Implementation of FBPNetworkProtocolManager

--- src/fr.irisa.diverse/java/FBPNetworkProtocol/FBPNetworkProtocolManager.java
+++ src/fr.irisa.diverse/java/FBPNetworkProtocol/FBPNetworkProtocolManager.java
@@ -7,10 +7,23 @@ import javax.websocket.MessageHandler;
  */
 public class FBPNetworkProtocolManager implements MessageHandler.Whole<String> {
 
+    // TODO : implement capabilities as described here : https://flowbased.github.io/fbp-protocol/#capabilities
+
     // Attributes
+    private NetworkMessageHandler network = null;
+    private GraphMessageHandler graph = null;
+    private ComponentMessageHandler component = null;
+    private RuntimeMessageHandler runtime = null;
+    private TraceMessageHandler trace = null;
 
     // Constructor
     public FBPNetworkProtocolManager () {
+
+        network = new NetworkMessageHandler(this);
+        graph = new GraphMessageHandler(this);
+        component = new ComponentMessageHandler(this);
+        runtime = new RuntimeMessageHandler(this);
+        trace = new TraceMessageHandler(this);
 
     }
 
@@ -18,7 +31,34 @@ public class FBPNetworkProtocolManager implements MessageHandler.Whole<String> {
                                    MessageHandler.Whole INTERFACE METHOD IMPLEMENTATION
        ===============================================================================================================*/
     @Override
-    public void onMessage(String message) {
+    public void onMessage(String msg) {
+        // Parse the received message add put it into a JSONObject.
+        FBPMessage message = new FBPMessage(msg);
+
+        String protocol = message.getProtocol();
+
+        // Redirect message to proper handler
+        switch (protocol) {
+            case "runtime" :
+                runtime.handleMessage(message);
+                break;
+            case "graph" :
+                graph.handleMessage(message);
+                break;
+            case "component" :
+                component.handleMessage(message);
+                break;
+            case "network" :
+                network.handleMessage(message);
+                break;
+            case "trace" :
+                trace.handleMessage(message);
+                break;
+            default :
+                System.err.println("Received message for unknown protocol : " + message.toJSONString());
+                break;
+        }
+
 
     }
 
