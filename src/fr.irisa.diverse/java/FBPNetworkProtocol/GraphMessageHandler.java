5826595abcff7728d9f506237ccd1a2856e0b779
Antoine Cheron <cheron.antoine@gmail.com>
2017-05-30 16:29:17 +0200

    Implemented received message handler for FBP graph protocol

--- src/fr.irisa.diverse/java/FBPNetworkProtocol/GraphMessageHandler.java
+++ src/fr.irisa.diverse/java/FBPNetworkProtocol/GraphMessageHandler.java
@@ -1,5 +1,11 @@
 package FBPNetworkProtocol;
 
+import Utils.JSON;
+import org.json.simple.JSONObject;
+import Flow.Flow;
+
+import java.util.ArrayList;
+
 /**
  * Created by antoine on 26/05/2017.
  */
@@ -7,10 +13,13 @@ public class GraphMessageHandler implements FBPProtocolHandler {
 
     // Attributes
     FBPNetworkProtocolManager owningManager;
+    Flow flow;
+    final String PROTOCOL = "graph";
 
     // Constructor
     public GraphMessageHandler (FBPNetworkProtocolManager manager) {
         this.owningManager = manager;
+        this.flow = owningManager.owningWorkspace.getFlow();
     }
 
     /* =================================================================================================================
@@ -21,14 +30,383 @@ public class GraphMessageHandler implements FBPProtocolHandler {
         String command = message.getCommand();
 
         switch (command) {
-            // TODO
+            case "clear" :
+                clear();
+                break;
+            case "addnode" :
+                addnode(message.getPayloadAsJSON());
+                break;
+            case "removenode" :
+                removenode(message.getPayloadAsJSON());
+                break;
+            case "renamenode" :
+                renamenode(message.getPayloadAsJSON());
+                break;
+            case "changenode" :
+                changenode(message.getPayloadAsJSON());
+                break;
+            case "addedge" :
+                addedge(message.getPayloadAsJSON());
+                break;
+            case "removeedge" :
+                removeedge(message.getPayloadAsJSON());
+                break;
+            case "changeedge" :
+                changeedge(message.getPayloadAsJSON());
+                break;
+            case "addinitial" :
+                addinitial(message.getPayloadAsJSON());
+                break;
+            case "removeinitial" :
+                removeinitial(message.getPayloadAsJSON());
+                break;
+            case "addinport" :
+                addinport(message.getPayloadAsJSON());
+                break;
+            case "removeinport" :
+                removeinport(message.getPayloadAsJSON());
+                break;
+            case "renameinport" :
+                renameinport(message.getPayloadAsJSON());
+                break;
+            case "addoutport" :
+                addoutport(message.getPayloadAsJSON());
+                break;
+            case "removeoutport" :
+                removeoutport(message.getPayloadAsJSON());
+                break;
+            case "renameoutport" :
+                renameoutport(message.getPayloadAsJSON());
+                break;
+            case "addgroup" :
+                addgroup(message.getPayloadAsJSON());
+                break;
+            case "removegroup" :
+                removegroup(message.getPayloadAsJSON());
+                break;
+            case "renamegroup" :
+                renamegroup(message.getPayloadAsJSON());
+                break;
+            case "changegroup" :
+                changegroup(message.getPayloadAsJSON());
+                break;
+            default :
+                owningManager.sendError(PROTOCOL, "Error with message : " + message.toJSONString());
+                break;
         }
-
     }
 
     /* =================================================================================================================
-                                                      PRIVATE METHODS
+                                   PRIVATE METHODS TO HANDLE RECEIVED MESSAGES
        ===============================================================================================================*/
+
+    private void clear () {
+        System.out.println("You are trying to empty the graph. It is too dangerous to be implemented");
+    }
+
+    private void addnode (JSONObject payload) {
+        // Retrieve needed data for addNode() method
+        String id = (String) payload.get("id");
+        String component = (String) payload.get("component");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.addNode(id, component, metadata, graph);
+
+        // Then it is the addNode() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void removenode (JSONObject payload) {
+        // Retrieve needed data for removeNode() method
+        String id = (String) payload.get("id");
+        String graph = (String) payload.get("graph");
+
+        flow.removeNode(id, graph);
+
+        // Then it is the removeNode() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void renamenode (JSONObject payload) {
+        // Retrieve needed data for renameNode() method
+        String from = (String) payload.get("from");
+        String to = (String) payload.get("to");
+        String graph = (String) payload.get("graph");
+
+        flow.renameNode(from, to, graph);
+
+        // Then it is the renameNode() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void changenode (JSONObject payload) {
+        // Retrieve needed data for changeNode() method
+        String id = (String) payload.get("id");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.changeNode(id, metadata, graph);
+
+        // Then it is the changeNode() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void addedge (JSONObject payload) {
+        // Retrieve needed data for addEdge() method
+        JSONObject src = (JSONObject) payload.get("src");
+        JSONObject tgt = (JSONObject) payload.get("tgt");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.addEdge(src, tgt, metadata, graph);
+
+        // Then it is the addEdge() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void removeedge (JSONObject payload) {
+        // Retrieve needed data for removeEdge() method
+        String graph = (String) payload.get("graph");
+        JSONObject src = (JSONObject) payload.get("src");
+        JSONObject tgt = (JSONObject) payload.get("tgt");
+
+        flow.removeEdge(graph, src, tgt);
+
+        // Then it is the removeEdge() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void changeedge (JSONObject payload) {
+        // Retrieve needed data for changeEdge() method
+        String graph = (String) payload.get("graph");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        JSONObject src = (JSONObject) payload.get("src");
+        JSONObject tgt = (JSONObject) payload.get("tgt");
+
+        flow.changeEdge(graph, metadata, src, tgt);
+
+        // Then it is the changeEdge() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void addinitial (JSONObject payload) {
+        // Retrieve needed data for addInitial() method
+        String graph = (String) payload.get("graph");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        JSONObject src = (JSONObject) payload.get("src");
+        JSONObject tgt = (JSONObject) payload.get("tgt");
+
+        flow.addInitial(graph, metadata, src, tgt);
+
+        // Then it is the addInitial() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void removeinitial (JSONObject payload) {
+        // Retrieve needed data for removeInitial() method
+        JSONObject src = (JSONObject) payload.get("src");
+        JSONObject tgt = (JSONObject) payload.get("tgt");
+        String graph = (String) payload.get("graph");
+
+        flow.removeInitial(graph, src, tgt);
+
+        // Then it is the removeInitial() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void addinport (JSONObject payload) {
+        // Retrieve needed data for addInport() method
+        String name = (String) payload.get("public");
+        String node = (String) payload.get("node");
+        String port = (String) payload.get("port");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.addInport(name, node, port, metadata, graph);
+
+        // Then it is the addInport() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void removeinport (JSONObject payload) {
+        // Retrieve needed data for removeInport() method
+        String name = (String) payload.get("public");
+        String graph = (String) payload.get("graph");
+
+        flow.removeInport(name, graph);
+
+        // Then it is the removeInport() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void renameinport (JSONObject payload) {
+        // Retrieve needed data for renameInport() method
+        String from = (String) payload.get("from");
+        String to = (String) payload.get("to");
+        String graph = (String) payload.get("graph");
+
+        flow.renameInport(from, to, graph);
+
+        // Then it is the renameInport() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void addoutport (JSONObject payload) {
+        // Retrieve needed data for addOutport() method
+        String name = (String) payload.get("public");
+        String node = (String) payload.get("node");
+        String port = (String) payload.get("port");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.addOutport(name, node, port, metadata, graph);
+
+        // Then it is the addOutport() method that will send a message to all UIs, after the modification has been done.
+
+    }
+
+    private void removeoutport (JSONObject payload) {
+        // Retrieve needed data for removeOutport() method
+        String name = (String) payload.get("public");
+        String graph = (String) payload.get("graph");
+
+        flow.removeOutport(name, graph);
+
+        // Then it is the removeOutport() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void renameoutport (JSONObject payload) {
+        // Retrieve needed data for renameOutport() method
+        String from = (String) payload.get("from");
+        String to = (String) payload.get("to");
+        String graph = (String) payload.get("graph");
+
+        flow.renameOutport(from, to, graph);
+
+        // Then it is the renameOutport() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void addgroup (JSONObject payload) {
+        // Retrieve needed data for addGroup() method
+        String name = (String) payload.get("name");
+        JSONObject nodes = (JSONObject) payload.get("nodes");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.addGroup(name, nodes, metadata, graph);
+
+        // Then it is the addGroup() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void removegroup (JSONObject payload) {
+        // Retrieve needed data for removeGroup() method
+        String name = (String) payload.get("name");
+        String graph = (String) payload.get("graph");
+
+        flow.removeGroup(name, graph);
+
+        // Then it is the removeGroup() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void renamegroup (JSONObject payload) {
+        // Retrieve needed data for renameGroup() method
+        String from = (String) payload.get("from");
+        String to = (String) payload.get("to");
+        String graph = (String) payload.get("graph");
+
+        flow.renameGroup(from, to, graph);
+
+        // Then it is the renameGroup() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    private void changegroup (JSONObject payload) {
+        // Retrieve needed data for changeGroup() method
+        String name = (String) payload.get("name");
+        JSONObject metadata = (JSONObject) payload.get("metadata");
+        String graph = (String) payload.get("graph");
+
+        flow.changeGroup(name, metadata, graph);
+
+        // Then it is the changeGroup() method that will send a message to all UIs, after the modification has been done.
+    }
+
+    /* =================================================================================================================
+                                            PRIVATE METHODS TO SEND MESSAGES
+       ===============================================================================================================*/
+
+    private void sendClearMessage () {
+        System.out.println("You are trying to empty the graph. It is too dangerous to be implemented");
+    }
+
+    private void sendAddNodeMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveNodeMessage (JSONObject payload) {
+
+    }
+
+    private void sendRenameNodeMessage (JSONObject payload) {
+
+    }
+
+    private void sendChangeNodeMessage (JSONObject payload) {
+
+    }
+
+    private void sendAddEdgeMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveEdgeMessage (JSONObject payload) {
+
+    }
+
+    private void sendChangeEdgeMessage (JSONObject payload) {
+
+    }
+
+    private void sendAddInitialMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveInitialMessage (JSONObject payload) {
+
+    }
+
+    private void sendAddInportMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveInportMessage (JSONObject payload) {
+
+    }
+
+    private void sendRenameInportMessage (JSONObject payload) {
+
+    }
+
+    private void sendAddOutportMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveOutportMessage (JSONObject payload) {
+
+    }
+
+    private void sendRenameOutportMessage (JSONObject payload) {
+
+    }
+
+    private void sendAddGroupMessage (JSONObject payload) {
+
+    }
+
+    private void sendRemoveGroupMessage (JSONObject payload) {
+
+    }
+
+    private void sendRenameGroupMessage (JSONObject payload) {
+
+    }
+
+    private void sendChangeGroupMessage (JSONObject payload) {
+
+    }
 
 }
 
