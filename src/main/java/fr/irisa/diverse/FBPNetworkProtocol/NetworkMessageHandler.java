5082fa6c7d5fd5f1889f3aca795d1e5ed15121df
Antoine Cheron <cheron.antoine@gmail.com>
2017-06-01 16:25:22 +0200

    Started implementing Network subprotocol of FBP-NP

--- src/main/java/fr/irisa/diverse/FBPNetworkProtocol/NetworkMessageHandler.java
+++ src/main/java/fr/irisa/diverse/FBPNetworkProtocol/NetworkMessageHandler.java
@@ -1,5 +1,10 @@
 package fr.irisa.diverse.FBPNetworkProtocol;
 
+import fr.irisa.diverse.Core.Workspace;
+import org.json.simple.JSONObject;
+
+import java.util.Date;
+
 /**
  * Created by antoine on 26/05/2017.
  */
@@ -49,7 +54,8 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
        ===============================================================================================================*/
 
     private void persist(FBPMessage message) {
-        // TODO : owningManager.respondToAllWith(createPersist());
+        // Might probably do something ... to do
+        sendPersistMessage(message);
     }
 
     private void getstatus(FBPMessage message) {
@@ -59,6 +65,37 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
     }
 
     private void start(FBPMessage message) {
+        JSONObject payload = message.getPayloadAsJSON();
+        String graph = (String) payload.get("graph");
+
+        boolean started = false;
+        long startTime = 0;
+
+        try {
+            // Send a network started message
+            startTime = sendStartedMessage(graph);
+            started = true;
+            // Actually start the graph
+            owningManager.owningWorkspace.startGraph(graph);
+            // Wait for the run to finish
+            while (owningManager.owningWorkspace.graphRunning(graph)) {
+                Thread.sleep(200);
+            }
+            // After it finishes : send a Stopped message
+            sendStoppedMessage(graph, startTime);
+
+        } catch (Workspace.NotExistingGraphException e) {
+            if (started) {
+                sendStoppedMessage(graph, startTime);
+            }
+            // Send the error to the clients
+            owningManager.sendError(PROTOCOL, e.getMessage());
+            // Print stack trace in stderr
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // Thread.sleep exception
+            e.printStackTrace();
+        }
     }
 
     private void stop(FBPMessage message) {
@@ -68,6 +105,42 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
                                             METHODS TO CREATE RESPONSES
        ===============================================================================================================*/
 
-    private FBPMessage createPersist (FBPMessage message) { return message; }
+    private void sendPersistMessage (FBPMessage message) {
+        owningManager.sendToAll(message);
+    }
+
+    private long sendStartedMessage (String graph) {
+        // Build the payload
+        JSONObject payload = new JSONObject();
+        Date date = new Date();
+        long time = date.getTime();
+        payload.put("time", time);
+        payload.put("graph", graph);
+        payload.put("started", true);
+        payload.put("running", true); // TODO
+        payload.put("debug", false); // TODO
+
+        // Send it
+        sendMessage("started", payload);
+
+        // Return the started time. For use when calling sendStoppedMessage methods
+        return time;
+    }
+
+    private void sendStoppedMessage (String graph, long time) {
+        // Build the payload
+        JSONObject payload = new JSONObject();
+        Date date = new Date();
+        long uptime = date.getTime() - time; // Not perfect calculation. Could be made better
+        payload.put("time", time);
+        payload.put("uptime", uptime);
+        payload.put("graph", graph);
+        payload.put("started", false);
+        payload.put("running", false); // TODO
+        payload.put("debug", false); // TODO
+
+        // Send it
+        sendMessage("started", payload);
+    }
 
 }
