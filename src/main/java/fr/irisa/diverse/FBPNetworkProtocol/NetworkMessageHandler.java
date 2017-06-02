618fe6ee5dcde964461371e34675b1e228a1cb8d
Antoine Cheron <cheron.antoine@gmail.com>
2017-06-02 11:54:45 +0200

    Continued network protocol implementation

--- src/main/java/fr/irisa/diverse/FBPNetworkProtocol/NetworkMessageHandler.java
+++ src/main/java/fr/irisa/diverse/FBPNetworkProtocol/NetworkMessageHandler.java
@@ -1,11 +1,14 @@
 package fr.irisa.diverse.FBPNetworkProtocol;
 
 import fr.irisa.diverse.Core.Workspace;
+import fr.irisa.diverse.FBPNetworkProtocolUtils.Status;
+import fr.irisa.diverse.Utils.Utils;
 import org.json.simple.JSONObject;
 
-import java.util.Date;
-
 /**
+ * Implementation of the handler for messages on the network sub-protocol of the FBP Network Protocol
+ * https://flowbased.github.io/fbp-protocol/#network-start
+ *
  * Created by antoine on 26/05/2017.
  */
 public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProtocolHandler {
@@ -42,6 +45,9 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
             case "stop" :
                 stop(message);
                 break;
+            case "debug" :
+                debug(message);
+                break;
             default:
                 System.err.println("[ERROR] Unknown message on Network : " + message.toJSONString());
                 break;
@@ -59,6 +65,10 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
     }
 
     private void getstatus(FBPMessage message) {
+        JSONObject payload = message.getPayloadAsJSON();
+        String graph = (String) payload.get("graph");
+
+        sendStatusMessage(graph);
     }
 
     private void edges(FBPMessage message) {
@@ -69,27 +79,26 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
         String graph = (String) payload.get("graph");
 
         boolean started = false;
-        long startTime = 0;
 
         try {
-            // Send a network started message
-            startTime = sendStartedMessage(graph);
-            started = true;
-            // Actually start the graph
+            // Start the graph
             owningManager.owningWorkspace.startGraph(graph);
+            started = true;
+            // Send a network started message
+            sendStartedMessage(graph);
             // Wait for the run to finish
             while (owningManager.owningWorkspace.graphRunning(graph)) {
                 Thread.sleep(200);
             }
             // After it finishes : send a Stopped message
-            sendStoppedMessage(graph, startTime);
+            sendStoppedMessage(graph);
 
         } catch (Workspace.NotExistingGraphException e) {
             if (started) {
-                sendStoppedMessage(graph, startTime);
+                sendStoppedMessage(graph);
             }
             // Send the error to the clients
-            owningManager.sendError(PROTOCOL, e.getMessage());
+            sendError(e.getMessage());
             // Print stack trace in stderr
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -99,6 +108,33 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
     }
 
     private void stop(FBPMessage message) {
+        JSONObject payload = message.getPayloadAsJSON();
+        // Retrieve the id of the graph
+        String graph = (String) payload.get("graph");
+
+        // Then we make sure the graph is really running
+        if (owningManager.owningWorkspace.graphRunning(graph)) {
+            // If so, we stop it
+            try {
+                owningManager.owningWorkspace.stopGraph(graph);
+                // Wait for the graph to finish
+                while (owningManager.owningWorkspace.graphRunning(graph)) {
+                    Thread.sleep(200);
+                }
+                // Send a stopped message after it finishes
+                sendStoppedMessage(graph);
+
+            } catch (InterruptedException e) {
+                // Thread.sleep exception
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void debug (FBPMessage message) {
+        // Not functional for now
+        sendError("Debug mode not functional");
     }
 
     /* =================================================================================================================
@@ -109,38 +145,60 @@ public class NetworkMessageHandler extends SendMessageOverFBP implements FBPProt
         owningManager.sendToAll(message);
     }
 
-    private long sendStartedMessage (String graph) {
+    private void sendStartedMessage (String graph) {
+        Status status = getGraphStatus(graph);
+
         // Build the payload
         JSONObject payload = new JSONObject();
-        Date date = new Date();
-        long time = date.getTime();
-        payload.put("time", time);
+        payload.put("time", String.valueOf(status.getStartedTime()));
         payload.put("graph", graph);
-        payload.put("started", true);
-        payload.put("running", true); // TODO
-        payload.put("debug", false); // TODO
-
-        // Send it
-        sendMessage("started", payload);
-
-        // Return the started time. For use when calling sendStoppedMessage methods
-        return time;
-    }
-
-    private void sendStoppedMessage (String graph, long time) {
-        // Build the payload
-        JSONObject payload = new JSONObject();
-        Date date = new Date();
-        long uptime = date.getTime() - time; // Not perfect calculation. Could be made better
-        payload.put("time", time);
-        payload.put("uptime", uptime);
-        payload.put("graph", graph);
-        payload.put("started", false);
-        payload.put("running", false); // TODO
-        payload.put("debug", false); // TODO
+        payload.put("started", status.hasStarted());
+        payload.put("running", status.isRunning());
+        payload.put("debug", status.isInDebugMode());
 
         // Send it
         sendMessage("started", payload);
     }
 
+    private void sendStoppedMessage (String graph) {
+        Status status = getGraphStatus(graph);
+
+        // Build the payload
+        JSONObject payload = new JSONObject();
+        payload.put("time", String.valueOf(status.getStoppedTime()));
+        payload.put("uptime", status.getUptime());
+        payload.put("graph", graph);
+        payload.put("started", status.hasStarted());
+        payload.put("running", status.isRunning());
+        payload.put("debug", status.isInDebugMode());
+
+        // Send it
+        sendMessage("stopped", payload);
+    }
+
+    private void sendStatusMessage (String graph) {
+        Status status = getGraphStatus(graph);
+
+        // Build the payload
+        JSONObject payload = new JSONObject();
+        payload.put("graph", graph);
+        payload.put("uptime", status.getUptime());
+        payload.put("started", status.hasStarted());
+        payload.put("running", status.isRunning());
+        payload.put("debug", status.isInDebugMode());
+
+        // Send it
+        sendMessage("status", payload);
+    }
+
+    /* =================================================================================================================
+                                            CLASS CUSTOM METHODS
+       ===============================================================================================================*/
+
+    private Status getGraphStatus (String graph) {
+        // Retrieve the graph
+        Object o = owningManager.owningWorkspace.getFlow().getGraph(graph);
+
+        return Utils.getGraphStatus(o);
+    }
 }
