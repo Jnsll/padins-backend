8def10787ad452155e7e5a74f39e2ebf65bd4ea9
AntoineCheron <cheron.antoine@gmail.com>
2017-07-20 16:51:29 +0200

    Add a run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar ./target/padins-0.1.0-BETA-jar-with-dependencies.jar
--- src/main/resources/retrieve-container-ip.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# $1 must be the container id
-docker inspect $1 | grep IPAddress | cut -d '"' -f 4
